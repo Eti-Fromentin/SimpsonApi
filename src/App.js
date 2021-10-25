b977e61be47b5a11ffe768a6a5185bb02ab13e96
Eti-Fromentin <fromentin.etienne@gmail.com>
2021-10-25 12:09:41 +0200

    quest done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [quote, SetQuote] = useState(null);
+  const [name, setName] = useState(null);
+  const [imgUrl, setImgUrl] = useState(null);
+
+  const GetQuote = () => {
+    axios
+      .get("https://simpsons-quotes-api.herokuapp.com/quotes")
+      .then((response) => response.data)
+      .then((data) => {
+        SetQuote(data[0].quote);
+        setName(data[0].character);
+        setImgUrl(data[0].image);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" onClick={GetQuote}>
+        Get a new quote
+      </button>
+      <p>{quote}</p>
+      <p>{name}</p>
+      <img src={imgUrl} alt={name} />
     </div>
   );
 }
